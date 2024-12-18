b33326f02f82457d392997fc2ecd5be7cb96df95
Shehrozkhannn <sharozekkhan@gmail.com>
2024-12-18 16:15:38 +0500

    Add products-card ui and other minor functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,ProductsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-    title = signal<string>('Store')
+    title = signal<string>('Store');
+
+
+    showBtnClicked(){
+      console.log('TEST BUTTON')
+    }
 }
